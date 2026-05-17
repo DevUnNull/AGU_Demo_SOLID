b84abcb5b91a8fe4a82f78f7a4c7c8d960bb92a3
HaiNMN <hai23092003@gmail.com>
2026-05-18 06:52:05 +0700

    update

---
+++ Assets/MyGame/Scrip/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private IWeapon weapon;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            weapon?.Shoot();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        IWeapon newWeapon = other.GetComponent<IWeapon>();
+
+        if (newWeapon != null)
+        {
+            weapon = newWeapon;
+
+            Debug.Log("Nhặt vũ khí: " + other.name);
+        }
+    }
+}
